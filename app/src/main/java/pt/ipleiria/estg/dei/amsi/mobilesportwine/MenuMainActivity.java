cfe476a78d10a2b9cebd10be961ef0e74665c959
Altamir Rodrigues <box.altamir@gmail.com>
2025-01-23 08:54:33 +0000

    Projeto Reiniciado com Email e Pass já definidos automaticamente

--- app/src/main/java/pt/ipleiria/estg/dei/amsi/mobilesportwine/MenuMainActivity.java
+++ app/src/main/java/pt/ipleiria/estg/dei/amsi/mobilesportwine/MenuMainActivity.java
@@ -1,17 +1,28 @@
 package pt.ipleiria.estg.dei.amsi.mobilesportwine;
 
-import static android.provider.ContactsContract.Intents.Insert.EMAIL;
-
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.util.Log;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MenuMainActivity extends AppCompatActivity {
     public static final String EMAIL = "pt.ipleiria.estg.dei.amsi.mobilesportwine.EMAIL";
+
+    private RecyclerView recyclerView;
+    //private ReviewAdapter adapter;
+   // private List<Review> reviewList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +37,36 @@ public class MenuMainActivity extends AppCompatActivity {
         } else {
             System.out.println("Nenhum email foi recebido.");
         }
+
+        //inicializando o RecyclerView
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+
+
+        //Buscar dados da API
+        fetchReviews();
+    }
+
+    private void fetchReviews() {
+        OkHttpClient client = new OkHttpClient();
+        String url ="http://51.20.254.239:8080//api/review/all?access-token=3BBK42bN2MNmHncBSm-otNLirjiYPGu1";
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+
+            }
+        });
     }
 }
